594e680370a91a9be90f90d0c692dcddb410934e
IslomAliy <kislombek71@gmail.com>
2022-02-07 12:55:26 +0500

    first commit

---
+++ pages/[car].js
@@ -0,0 +1,29 @@
+import cars from '../data/cars.json'
+import Link from 'next/link'
+
+
+export default function Car({car}) {
+    return <div style={{backgroundColor: car.hex, padding: 20}}> 
+        <h1 style={{color: car.textColor}}> {car.name} </h1>
+        <Link href="/">
+            <h4 style={{cursor: 'pointer'}}> Go home </h4>
+        </Link>
+    </div>
+}
+
+export async function getStaticPaths() {
+    const paths = cars.map(car => ({
+            params: {car: car.name }
+    }))
+    return {
+        paths,
+        fallback: true
+    }
+} 
+
+export async function getStaticProps({params}) {
+    const car = cars.find(car => car.name == params.car)
+    return {
+        props: {car}
+    }
+}
